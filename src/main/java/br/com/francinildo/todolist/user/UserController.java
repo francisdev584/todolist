3f1d349822957967c9c4483e1429c6c32139105c
Francinildo Rodrigues <francis584@gmail.com>
2025-02-17 18:11:59 -0300

    create POST request to create a new user

--- src/main/java/br/com/francinildo/todolist/user/UserController.java
+++ src/main/java/br/com/francinildo/todolist/user/UserController.java
@@ -1,8 +1,13 @@
 package br.com.francinildo.todolist.user;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,5 +23,19 @@ public class UserController {
     @GetMapping("/")
     public List<UserModel> findAllUsers() {
         return this.userRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
