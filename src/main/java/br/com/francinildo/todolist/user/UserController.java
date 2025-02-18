9a0e3366fd0e87ba0ad13d41bc1f03778f2cc714
Francinildo Rodrigues <francis584@gmail.com>
2025-02-18 18:35:45 -0300

    fix: add @RestController to UserController

--- src/main/java/br/com/francinildo/todolist/user/UserController.java
+++ src/main/java/br/com/francinildo/todolist/user/UserController.java
@@ -1,18 +1,18 @@
 package br.com.francinildo.todolist.user;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
