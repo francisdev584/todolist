79010e8cd6d0c745699ab8c3719ac7146c78fd82
Francinildo Rodrigues <francis584@gmail.com>
2025-02-18 19:22:34 -0300

    feat: set idUser from the userEntity

--- src/main/java/br/com/francinildo/todolist/task/TaskController.java
+++ src/main/java/br/com/francinildo/todolist/task/TaskController.java
@@ -1,11 +1,15 @@
 package br.com.francinildo.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -17,7 +21,9 @@ public class TaskController {
     }
 
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest servletRequest) {
+        var idUser = servletRequest.getAttribute("idUser");
+        taskModel.setId((UUID)idUser);
         TaskModel taskCreated = this.taskRepository.save(taskModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
