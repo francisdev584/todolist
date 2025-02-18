8d9f4be4fcc86983b850dc35286e34b4622f0d27
Francinildo Rodrigues <francis584@gmail.com>
2025-02-18 18:36:03 -0300

    feat: create TaskController

---
+++ src/main/java/br/com/francinildo/todolist/task/TaskController.java
@@ -0,0 +1,25 @@
+package br.com.francinildo.todolist.task;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final ITaskRepository taskRepository;
+
+    public TaskController(ITaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+        TaskModel taskCreated = this.taskRepository.save(taskModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+}
